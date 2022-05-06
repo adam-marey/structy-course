1ffeaadae4bc44026a39bbc455e86bb154d0fe62
Sulaiman Marey <adammarey51@gmail.com>
2022-05-05 22:54:57 -0700

    add compress solution

---
+++ compress.js
@@ -0,0 +1,44 @@
+/*
+  Write a function, compress, that takes in a string as an argument.
+  The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character.
+  Single character occurrences should not be changed.
+
+  'aaa' compresses to '3a'
+  'cc' compresses to '2c'
+  't' should remain as 't'
+
+
+  compress('ccaaatsss'); // -> '2c3at3s'
+  compress('ssssbbz'); // -> '4s2bz'
+  compress('ppoppppp'); // -> '2po5p'
+  compress('nnneeeeeeeeeeeezz'); // -> '3n12e2z'
+*/
+const compress = (s) => {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (j <= s.length) {
+    if (s[i] === s[j]) {
+      j += 1;
+    } else {
+      const num = j - i;
+      if (num === 1) {
+        result.push(s[i]);
+      } else {
+        result.push(String(num), s[i]);
+      }
+      i = j;
+    }
+  }
+
+  return result.join('');
+};
+
+module.exports = {
+  compress,
+};
+
+// n = length of string
+// Time: O(n)
+// Space: O(n)
