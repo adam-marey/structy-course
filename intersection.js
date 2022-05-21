c1c7046742b3b2fe1542767fb22372e7ed756ae4
Sulaiman Marey <adammarey51@gmail.com>
2022-05-20 20:26:06 -0700

    added intersection solution problem

---
+++ intersection.js
@@ -0,0 +1,31 @@
+/*
+intersection
+
+Write a function, intersection, that takes in two arrays, a,b, as arguments.
+The function should return a new array containing elements that are in both of the two arrays.
+You may assume that each input array does not contain duplicate elements.
+
+intersection([4,2,1,6], [3,6,9,2,10]) // -> [2,6]
+intersection([2,4,6], [4,2]) // -> [2,4]
+intersection([4,2,1], [1,2,4,6]) // -> [1,2,4]
+intersection([0,1,2], [10,11]) // -> []
+
+*/
+
+
+// using set
+const intersection = (a, b) => {
+  const result = [];
+  const setA = new Set(a);
+  for (let item of b) {
+    if (setA.has(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+
+// n = length of array a, m = length of array b
+// Time: O(n+m)
+// Space: O(n)
